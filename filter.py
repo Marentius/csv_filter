83a9d3f082c3401a10287387b825977bb3c65fef
Marentius <vetlenilseen@hotmail.com>
2024-09-10 19:13:39 +0200

    la til data for california

--- filter.py
+++ filter.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Yo peeps. Dette skriptet leser inn en csv-fil og filtrerer ut de kolonnene vi vil bruke.
 
 #For å bruke scriptet må dere huske å endre input_file_path tilpasset deres lokale filsti. 
-input_file_path = 'C:\\Users\\vetle\\OneDrive\\Dokumenter\\Skole\\BigData\\raw\\psam_p23.csv'
+input_file_path = 'C:\\Users\\vetle\\OneDrive\\Dokumenter\\Skole\\BigData\\raw\\psam_p06.csv'
 output_file_path = 'filtrert.csv'  # Dette er output-fila 
 
 # Her kan dere definere hvilke kolonner dere vil beholde 
