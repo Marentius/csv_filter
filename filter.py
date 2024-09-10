4ddf611776f1530f4f53218bd28c5a2350749d14
Marentius <vetlenilseen@hotmail.com>
2024-09-10 19:26:16 +0200

    Verdier konverteres nå til INT

--- filter.py
+++ filter.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Yo peeps. Dette skriptet leser inn en csv-fil og filtrerer ut de kolonnene vi vil bruke.
 
-#For å bruke scriptet må dere huske å endre input_file_path tilpasset deres lokale filsti. 
+# For å bruke scriptet må dere huske å endre input_file_path tilpasset deres lokale filsti. 
 input_file_path = 'C:\\Users\\vetle\\OneDrive\\Dokumenter\\Skole\\BigData\\raw\\psam_p06.csv'
 output_file_path = 'filtrert.csv'  # Dette er output-fila 
 
@@ -12,9 +12,8 @@ columns_to_keep = ['SCHL', 'SCHG', 'POVPIP', 'WKL', 'FOD1P']
 # Ikke rør noe under her
 df = pd.read_csv(input_file_path)
 
-
-filtered_df = df[columns_to_keep]
-
+# Filtrer kolonnene og fyll NA-verdier med 0 før konvertering til int
+filtered_df = df[columns_to_keep].fillna(0).astype(int)
 
 filtered_df.to_csv(output_file_path, index=False)
 
